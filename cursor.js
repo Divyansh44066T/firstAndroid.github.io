df69c315e6a477a4a6d92bec3caf742de846cd6b
Divyansh44066T <90370110+Divyansh44066T@users.noreply.github.com>
2021-10-09 17:44:54 +0530

    Add files via upload

---
+++ cursor.js
@@ -0,0 +1,35 @@
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
+const section = document.querySelectorAll('section');
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+// Move the cursor
+function onMouseMove(e) {
+  TweenMax.to($bigBall, .40, {
+    x: e.pageX - 45,
+    y: e.pageY - 45
+  })
+  TweenMax.to($smallBall, .90, {
+    x: e.pageX - 5,
+    y: e.pageY - 0
+  })
+}
+
+// Hover an element
+function onMouseHover() {
+  TweenMax.to($bigBall, .3, {
+    scale: 4
+  })
+}
+function onMouseHoverOut() {
+  TweenMax.to($bigBall, .3, {
+    scale: 1
+  })
+}
